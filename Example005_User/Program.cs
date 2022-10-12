d1ea9f7ade7a1be1f88980b614e483b877050ca0
grisha <grisha.zhivot@bk.ru>
2022-10-12 16:06:33 +0300

    user

---
+++ Example005_User/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите имя: ");
+string usernume=Console.ReadLine();
+
+if(usernume.ToLower() == "вика")
+{
+    Console.WriteLine("Приветик солнышко");
+}
+if(usernume.ToLower() =="платон")
+{
+    Console.WriteLine("Пошол нахуй шлюха");
+}
+else
+{
+    Console.Write("Здраствуйте, ");
+    Console.WriteLine(usernume);
+}
+
