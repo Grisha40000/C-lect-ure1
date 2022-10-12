ab67f6a22b48083cffe233612ac9c036785d8349
grisha <grisha.zhivot@bk.ru>
2022-10-12 22:01:57 +0300

    triangle2

--- Example006_triangle/Program.cs
+++ Example006_triangle/Program.cs
@@ -1,5 +1,4 @@
-﻿
- Console.Clear();
+﻿Console.Clear();
 //Console.SetCursorPosition(10,4);
 //Console.WriteLine(".");
 int xa = 40;
@@ -21,7 +20,7 @@ Console.WriteLine(".");
 int x = xa, y = xb;
 
 int count = 0;
-while (count < 10000)
+while (count < 100000)
 {
     int what = new Random().Next(0, 3);
 
@@ -48,4 +47,5 @@ while (count < 10000)
     Console.WriteLine(".");
     
 }
+
 
